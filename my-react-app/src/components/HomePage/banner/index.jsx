e1dc251c7a8a9c5cf1877344834b8445982dc508
Harry Rodrigues <harryrodriads@gmail.com>
2023-11-24 18:19:42 -0300

    v1.1

--- my-react-app/src/components/HomePage/banner/index.jsx
+++ my-react-app/src/components/HomePage/banner/index.jsx
@@ -10,9 +10,9 @@ export const Banner = () => {
         <img src={bannerworkers } alt="" />
       </figure>
       <div className={styles.divPrimary}>
-        <h1 className="titlebanner">Nós preparamos</h1>
-        <h1 className="titlebanner">para o <span>futuro!</span> </h1>
-        <h4 className="textbanner">Oferecemos o melhor projeto arquitetônico, construção e serviços de manutenção predial para você.</h4>
+        <h1 className="titlebanner">Construindo Sonhos,</h1>
+        <h1 className="titlebanner">realizando <span>Vidas!</span> </h1>
+        <h4 className="textbanner"><br></br>Oferecemos o melhor projeto arquitetônico, construção e serviços de manutenção para você.</h4>
       </div>
       <div className={styles.divSecundary}> 
         <div  className={styles.card}>
@@ -27,15 +27,15 @@ export const Banner = () => {
       <div className={styles.divBox}>
           <div className={styles.contentCard}>
             <img src={Check} alt="Check" className="checkLogo" />
-            <h3 className="textCardBox">Sistema de controle de qualidade, garantia de 100% de satisfação</h3>
+            <h3 className="textCardBox">Sistema de controle de qualidade, garantia de 100% de satisfação;</h3>
           </div>
           <div className={styles.contentCard}>
             <img src={Check} alt="Check" className="checkLogo" />
-            <h3 className="textCardBox">Equipe altamente profissional, processos de teste precisos</h3>
+            <h3 className="textCardBox">Equipe altamente profissional, processos de teste precisos;</h3>
           </div>
           <div className={styles.contentCard}>
             <img src={Check} alt="Check" className="checkLogo" />
-            <h3 className="textCardBox">Mão de obra incomparável, profissional e qualificada</h3>
+            <h3 className="textCardBox">Mão de obra incomparável, profissional e qualificada.</h3>
           </div>
       </div>
     </section>
