d10772884dea1ceb41b17834a2baae69cfe599ef
Harry Rodrigues <harryrodriads@gmail.com>
2023-11-25 13:55:17 -0300

    v1.1.4

--- my-react-app/src/components/JenniferResid/sectionCarrosel/carroselImg/index.jsx
+++ my-react-app/src/components/JenniferResid/sectionCarrosel/carroselImg/index.jsx
@@ -28,7 +28,7 @@ export const Carousel = () => {
   const images = [img1, img2, img3, img5, img6, img7, img8, img9, img10, img11, img12, img13, img14, img15, img16, img17, img18, img19, img20, img21,]; // Adicione aqui as URLs das suas imagens
 
   const settings = {
-    dots: true,
+    dots: false,
     infinite: true,
     speed: 500,
     slidesToShow: 1,
